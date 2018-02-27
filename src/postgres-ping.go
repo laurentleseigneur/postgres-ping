da21e2977089a81487b85096e4a95e9705a3a0b3
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2018-02-27 15:11:46 +0100

    use defer and extract error check code

--- src/postgres-ping.go
+++ src/postgres-ping.go
@@ -15,45 +15,41 @@ func main() {
 	if argsCount != 6 {
 		panic("wrong parameters count. Expected parameters are: host, port, dbname, user, password")
 	}
-
 	host := args[1]
-
 	port, err := strconv.ParseInt(args[2], 10, 0)
-	if err != nil {
-		panic(err)
-	}
-
+	checkAndPanic(err)
 	dbname := args[3]
 	user := args[4]
 	password := args[5]
-
-	message := fmt.Sprintf("Checking connection to %s:%d/%s with user %s ",
-		host, port, dbname, user)
-	fmt.Println(message)
-
+	fmt.Printf("Checking connection to %s:%d/%s with user %s\n", host, port, dbname, user)
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", user, password, host, port, dbname)
+
 	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
+	checkAndPanic(err)
+	defer closeDb(db)
 
 	rows, err := db.Query("select 1")
-	if err != nil {
-		panic(err)
-	}
-
+	checkAndPanic(err)
 	defer rows.Close()
+
 	for rows.Next() {
 		var id int
 		var name string
 		err = rows.Scan(&id, &name)
-
 	}
 	err = rows.Err() // get any error encountered during iteration
+	checkAndPanic(err)
+	fmt.Println("Successfully connected!")
+}
+
+func checkAndPanic(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
 
-	fmt.Println("Successfully connected!")
-	db.Close()
+func closeDb(db *sql.DB) {
+	err := db.Close()
+	checkAndPanic(err)
+	println("DB Connection closed.")
 }
